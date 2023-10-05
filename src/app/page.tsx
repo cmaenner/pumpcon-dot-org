93ec371c564109fcf16780fa2c9c0760fc07136b
Chris Maenner <3287968+cmaenner@users.noreply.github.com>
2023-10-05 00:57:36 -0400

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,89 @@
+import Image, { type ImageProps } from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  TwitterIcon,
+} from '@/components/SocialIcons'
+import image1 from '@/images/photos/image-1.jpg'
+import image2 from '@/images/photos/image-2.jpg'
+import image3 from '@/images/photos/image-3.jpg'
+import image4 from '@/images/photos/image-4.jpg'
+import image5 from '@/images/photos/image-5.jpg'
+
+function SocialLink({
+  icon: Icon,
+  ...props
+}: React.ComponentPropsWithoutRef<typeof Link> & {
+  icon: React.ComponentType<{ className?: string }>
+}) {
+  return (
+    <Link className="group -m-1 p-1" {...props}>
+      <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+    </Link>
+  )
+}
+
+function Photos() {
+  let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+
+  return (
+    <div className="mt-16 sm:mt-20">
+      <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
+        {[image1, image2, image3, image4, image5].map((image, imageIndex) => (
+          <div
+            key={image.src}
+            className={clsx(
+              'relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+              rotations[imageIndex % rotations.length],
+            )}
+          >
+            <Image
+              src={image}
+              alt=""
+              sizes="(min-width: 640px) 18rem, 11rem"
+              className="absolute inset-0 h-full w-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default async function Home() {
+  // let articles = (await getAllArticles()).slice(0, 4)
+
+  return (
+    <>
+      <Container className="mt-9">
+        <div className="max-w-2xl">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
+            Pumpcon Turns DIRTY THIRTY!
+          </h1>
+          <p>
+          Consider yourself invited to Pumpcon 2023... (cant touch this)
+          </p>
+          <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+          Early Friday Night, October 20th till barely after midnight Sunday, October 22nd 2023 in a dungeon in Philly Details to be announced in September along with the t-shirt design but expect us to be at the same bowling alley on Friday and same sex dungeon on Saturday as in 2022...
+          </p>
+          <div className="mt-6 flex gap-6">
+            <SocialLink
+              href="https://twitter.com/pumpcon"
+              aria-label="Follow on Twitter"
+              icon={TwitterIcon}
+            />
+            <SocialLink
+              href="https://github.com/cmaenner/pumpcon-dot-org"
+              aria-label="Follow on GitHub"
+              icon={GitHubIcon}
+            />
+          </div>
+        </div>
+      </Container>
+      <Photos />
+    </>
+  )
+}
